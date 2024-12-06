bd5f50f363326bd46aea3c7aa7b73614ec0ea6c5
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-12-06 12:47:00 -0500

    Initial Commit

---
+++ SptoifyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SptoifyMusic
+//
+//  Created by Виталий Сухинин on 06.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
